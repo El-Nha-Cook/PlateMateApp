ca43604fa6780dd7dbafdab6e5d1fdfc29ce1ae1
nha pham <78373874+nhapham03@users.noreply.github.com>
2026-05-17 15:27:16 -0700

    feat: implement navigation header and optimize router with lazy loading

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -1,31 +1,32 @@
 <script setup lang="ts">
-import { RouterView } from "vue-router";
+import { RouterView } from 'vue-router'
+import Header from '@/layouts/Header.vue'
 </script>
 
 <template>
-    <div class="layout-wrapper">
-        <!-- header component  -->
-        <!-- nav component -->
-        <main class="main-layout">
-            <RouterView />
-        </main>
-        <!-- footer component  -->
-    </div>
+  <div class="layout-wrapper">
+    <!-- header component  -->
+    <Header />
+    <!-- nav component -->
+    <main class="main-layout">
+      <RouterView />
+    </main>
+    <!-- footer component  -->
+  </div>
 </template>
 
 <style>
 /* temporary styling  */
 .layout-wrapper {
-    margin: 0;
-    padding: 0;
+  margin: 0;
+  padding: 0;
 }
 .main-layout {
-    display: flex;
-    flex-direction: column;
-    background-color: gainsboro;
-    align-items: center;
-    flex: 1;
-    gap: 1rem;
-
+  display: flex;
+  flex-direction: column;
+  background-color: gainsboro;
+  align-items: center;
+  flex: 1;
+  gap: 1rem;
 }
 </style>
