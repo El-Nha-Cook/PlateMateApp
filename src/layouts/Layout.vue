84a698329b1a446e4ad5d2950d14d92124ca7da8
El Brewster <167570227+elbi3@users.noreply.github.com>
2026-05-18 18:56:35 -0700

    Move layout styling into global CSSfile

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -14,18 +14,4 @@ import { RouterView } from "vue-router";
 </template>
 
 <style>
-/* temporary styling  */
-.layout-wrapper {
-    margin: 0;
-    padding: 0;
-}
-.main-layout {
-    display: flex;
-    flex-direction: column;
-    background-color: gainsboro;
-    align-items: center;
-    flex: 1;
-    gap: 1rem;
-
-}
 </style>
--- src/layouts/RecipesLayout.vue
+++ src/layouts/RecipesLayout.vue
@@ -15,18 +15,4 @@ import { RouterView } from "vue-router";
 </template>
 
 <style>
-/* temporary styling  */
-.recipes-layout-wrapper {
-    margin: 0;
-    padding: 0;
-}
-.recipes-layout {
-    display: flex;
-    flex-direction: column;
-    background-color: gainsboro;
-    align-items: center;
-    flex: 1;
-    gap: 1rem;
-
-}
 </style>
