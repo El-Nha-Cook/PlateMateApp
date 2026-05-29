d9aae823455aa16d28616e8efccb45b06b174696
nha pham <78373874+nhapham03@users.noreply.github.com>
2026-05-29 12:09:01 -0700

    Updated Layout file

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -17,18 +17,4 @@ import Footer from '@/layouts/Footer.vue'
   </div>
 </template>
 
-<style>
-/* temporary styling  */
-.layout-wrapper {
-  margin: 0;
-  padding: 0;
-}
-.main-layout {
-  display: flex;
-  flex-direction: column;
-  background-color: gainsboro;
-  align-items: center;
-  flex: 1;
-  gap: 1rem;
-}
-</style>
+<style></style>
