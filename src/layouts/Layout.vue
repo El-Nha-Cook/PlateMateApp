30c7f72d39cc2955fb3eb9cc53adc7cd74da1af8
nha pham <78373874+nhapham03@users.noreply.github.com>
2026-05-29 12:02:15 -0700

    Update Layout structure

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import Header from '@/layouts/Header.vue'
+import Footer from '@/layouts/Footer.vue'
 </script>
 
 <template>
@@ -12,6 +13,7 @@ import Header from '@/layouts/Header.vue'
       <RouterView />
     </main>
     <!-- footer component  -->
+    <Footer />
   </div>
 </template>
 
