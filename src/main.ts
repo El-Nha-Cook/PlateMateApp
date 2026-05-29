51f42ce8c75a3f4a5bc863b38ede8c6a20450649
El Brewster <167570227+elbi3@users.noreply.github.com>
2026-05-29 11:35:46 -0700

    Install and add pinia colada to the project in main ts file

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import "@/styles/global.css"
 import { createPinia } from 'pinia'
+import { PiniaColada } from "@pinia/colada"
 import router from '@/router/index'
 
 
 const app = createApp(App)
-
 app.use(createPinia())
+app.use(PiniaColada, {
+  queryOptions: {
+    staleTime: 0,
+  },
+})
+
 app.use(router)
 
 app.mount('#app')
