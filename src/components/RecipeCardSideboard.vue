4859f7ace1a2ee133ac10b9d3e8b44546ffbfaaf
El Brewster <167570227+elbi3@users.noreply.github.com>
2026-06-06 10:28:18 -0700

    Add DailyMenuPlanner test and RecipeCardSideboard component

---
+++ src/components/RecipeCardSideboard.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+// import { makeDraggable } from "@vue-dnd-kit/core";
+import DraggableCard from "./DraggableCard.vue";
+
+defineProps({ cards: Array});
+const emit = defineEmits(["move-to-dashboard"]);
+</script>
+
+<template>
+    <aside class="sideboard-aside">
+        <h3>sideboard</h3>
+        <DraggableCard 
+            v-for="card in cards" 
+            :key="card.id"
+            :card="card"
+            origin="sideboard"
+            @click="emit('move-to-dashboard', card.id)"
+        />
+    </aside>
+</template>
+
+<style>
+.sideboard-aside {
+    background-color: aqua;
+    border: 1px solid blue;
+    padding: 1rem;
+}
+</style>
