11e28bef1db259f4746971af7bd83090ad9b9db7
El Brewster <167570227+elbi3@users.noreply.github.com>
2026-06-06 10:30:06 -0700

    Add MenuDashbaord component

---
+++ src/components/MenuDashboard.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import BucketZone from './BucketZone.vue';
+
+const props = defineProps({ 
+    plannerCards: Object,
+    moveToDashboard: Function, //moveToDashboard passed down from parent
+    moveToSideboard: Function,
+    moveBetweenBuckets: Function
+}); //defineProps compiler macro takes an options object
+
+const buckets = [
+  { key: 'morning',   label: '🌅 Morning'   },
+  { key: 'midday',    label: '☀️ Midday'    },
+  { key: 'afternoon', label: '🌤 Afternoon' },
+  { key: 'evening',   label: '🌙 Evening'   },
+  { key: 'nightowl',  label: '🦉 NightOwl'  },
+];
+
+</script>
+
+<template>
+    <section class="menu-dashboard-section">
+        <h3>Menu Dashboard</h3>
+        <BucketZone
+        v-for="bucket in buckets"
+        :bucket-key="bucket.key"
+        :label="bucket.label"
+        :cards="plannerCards[bucket.key]"
+        :move-to-dashboard="moveToDashboard"
+        :move-to-sideboard="moveToSideboard"
+        :move-between-buckets="moveBetweenBuckets"
+        />
+    </section>
+</template>
+
+<style>
+.menu-dashboard-section {
+    background-color: thistle;
+    border: 1px solid salmon;
+    padding: 1rem;
+}
+.menu-dashboard-section.is-overed{
+    outline: 5px solid salmon;
+}
+</style>
