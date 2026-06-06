896e5f1eee8bbad9ec5d3e265e3420208d55a097
El Brewster <167570227+elbi3@users.noreply.github.com>
2026-06-06 10:28:56 -0700

    Add DraggableCard component

---
+++ src/components/DraggableCard.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { makeDraggable } from '@vue-dnd-kit/core';
+import { useTemplateRef } from 'vue';
+
+const props = defineProps({ 
+    card: Object,
+    origin: String
+});
+
+const emit = defineEmits(['click', 'remove']);
+
+const cardRef = useTemplateRef('cardRef');
+
+makeDraggable(cardRef, {
+    //this is the payload the drop zone reads through e.draggingElements[0].data
+    data: () => ({ id: props.card.id, origin: props.origin})
+});
+</script>
+
+<template>
+    <div
+        ref="cardRef"
+        style="touch-action: none;"
+        @click="emit('click')"
+    >
+    <span>{{ card.emoji }} {{ card.name }}</span>
+    <button v-if="origin === 'planner'" @click.stop="emit('remove')">x</button>
+    <button v-else @click.stop="emit('click')">Add -></button>
+    </div>
+</template>
