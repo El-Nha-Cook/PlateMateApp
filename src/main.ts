2a9b6b85058bb9bac17b9fcec08733b301e7be60
El Brewster <167570227+elbi3@users.noreply.github.com>
2026-06-06 11:07:12 -0700

    Add pinia persisted state install and apply to main ts file

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,22 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import "@/styles/global.css"
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import { PiniaColada } from "@pinia/colada"
 import router from '@/router/index'
 
 
 const app = createApp(App)
-app.use(createPinia())
+
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate); //adds localStorage persistence
+
+app.use(pinia)
 app.use(PiniaColada, {
   queryOptions: {
     staleTime: 0,
   },
 })
-
 app.use(router)
 
 app.mount('#app')
